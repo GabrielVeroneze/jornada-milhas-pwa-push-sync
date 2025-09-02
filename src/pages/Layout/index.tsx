bab695a8d410c4f673bf9830cc544c5b732967c0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-09-02 15:36:02 -0300

    ✨ feat: adiciona hook useNotification ao componente Layout
    Importa e inicializa o hook useNotification no componente Layout, preparando para o tratamento futuro de notificações.

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router'
+import { useNotification } from '@/hooks/useNotification'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 const Layout = () => {
+    const {} = useNotification()
+
     return (
         <>
             <Header />
