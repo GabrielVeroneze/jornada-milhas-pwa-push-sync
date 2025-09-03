cacdf1b7767a8989ac14ce444e4cd562cef4446a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-09-03 15:35:38 -0300

    ✨ feat: adiciona configuração e inicialização do Firebase
    Adiciona o `firebase.ts` com a inicialização do aplicativo Firebase e a configuração de mensagens.

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getMessaging } from 'firebase/messaging'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyDdXBlA94GA2RamfpFM3dq_ms3I_fnVeTI',
+    authDomain: 'jornada-milhas-f8767.firebaseapp.com',
+    projectId: 'jornada-milhas-f8767',
+    storageBucket: 'jornada-milhas-f8767.firebasestorage.app',
+    messagingSenderId: '770233263649',
+    appId: '1:770233263649:web:6c2c5574d0fad61d952560',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const messaging = getMessaging()
