597047c429eca7ca7f77bfd5ccb92a710066c5c8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-09-04 17:20:42 -0300

    ✨ feat: migra o service worker do Firebase para API compatível com JS
    Renomeia `firebase-messaging-sw.ts` para `firebase-messaging-sw.js` e troca importações de módulos ES por importScripts com bibliotecas compatíveis com o Firebase.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,25 +1,23 @@
-import { initializeApp } from 'firebase/app'
-import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw'
+importScripts('https://www.gstatic.com/firebasejs/12.2.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/12.2.0/firebase-messaging-compat.js')
 
-const firebaseConfig = {
+firebase.initializeApp({
     apiKey: 'AIzaSyDdXBlA94GA2RamfpFM3dq_ms3I_fnVeTI',
     authDomain: 'jornada-milhas-f8767.firebaseapp.com',
     projectId: 'jornada-milhas-f8767',
     storageBucket: 'jornada-milhas-f8767.firebasestorage.app',
     messagingSenderId: '770233263649',
     appId: '1:770233263649:web:6c2c5574d0fad61d952560',
-}
+})
 
-initializeApp(firebaseConfig)
+const messaging = firebase.messaging()
 
-const messaging = getMessaging()
-
-onBackgroundMessage(messaging, (payload) => {
+messaging.onBackgroundMessage((payload) => {
     console.log('Notificação em segundo plano', payload)
 
-    const notificationTitle = payload.notification?.title || 'Nova notificação'
+    const notificationTitle = payload.notification.title
     const notificationOptions = {
-        body: payload.notification?.body,
+        body: payload.notification.body,
     }
 
     self.registration.showNotification(notificationTitle, notificationOptions)
