20a7cf9dec1c8072f0e16fb6f41733f7d78aa3c1
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-09-05 16:16:10 -0300

    ✨ feat: integra notificações do Firebase no hook `useFirebaseNotification`
    Atualiza `useFirebaseNotification` para escutar mensagens em tempo real usando `onMessageListener` do Firebase Messaging e atualiza o estado de `notification`
    exibe notificações com toast e ToastDisplay sempre que title estiver definido.

---
+++ src/hooks/useFirebaseNotification.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import { onMessageListener } from '@/firebase'
+import type { MessagePayload } from 'firebase/messaging'
+import type { Notification } from '@/types/Notification'
+import ToastDisplay from '@/components/ToastDisplay'
+
+export const useFirebaseNotification = () => {
+    const [notification, setNotification] = useState<Notification>({
+        title: '',
+        body: '',
+    })
+
+    useEffect(() => {
+        const notify = () => toast(<ToastDisplay notification={notification} />)
+
+        if (notification.title) {
+            notify()
+        }
+    }, [notification])
+
+    useEffect(() => {
+        const handleMessage = (payload: MessagePayload) => {
+            setNotification({
+                title: payload.notification?.title,
+                body: payload.notification?.body,
+            })
+        }
+
+        onMessageListener()
+            .then(handleMessage)
+            .catch(error => console.log(error))
+    }, [])
+
+    return {}
+}
